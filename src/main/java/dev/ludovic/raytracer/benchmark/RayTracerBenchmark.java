1e05e4858790026a2dd5aea51b30f5ef8d00b9c6
Ludovic Henry <git@ludovic.dev>
2021-04-09 22:39:37 +0200

    Add parameters for benchmark

--- src/main/java/dev/ludovic/raytracer/benchmark/RayTracerBenchmark.java
+++ src/main/java/dev/ludovic/raytracer/benchmark/RayTracerBenchmark.java
@@ -6,6 +6,7 @@ import java.io.Writer;
 import java.util.concurrent.ExecutionException;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
@@ -16,12 +17,15 @@ import dev.ludovic.raytracer.*;
 public class RayTracerBenchmark {
 
     private final Scene scene = Scene.random();
-    private final double aspect_ratio = 3.0 / 2.0;
-    private final int image_width = 300;
-    private final int image_height = (int)(image_width / aspect_ratio);
+
+    @Param({ "1.5" })
+    public double ratio;
+
+    @Param({ "100" })
+    public int height;
 
     @Benchmark
     public void render(Blackhole bh) throws InterruptedException, ExecutionException {
-        bh.consume(scene.render(image_width, image_height));
+        bh.consume(scene.render((int)(height * ratio), height));
     }
 }
